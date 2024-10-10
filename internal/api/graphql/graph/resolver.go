d7a1fa017222af177cb20c7845bc7ce13f2e83f5
dyianvo <nestkyo82@gmail.com>
2024-10-10 21:36:44 +0700

    Update: module path

---
+++ internal/api/graphql/graph/resolver.go
@@ -0,0 +1,13 @@
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+package graph
+
+import( 
+	
+)
+
+
+type Resolver struct{
+	
+}
